85ec690636556d082eac3add70605ef68f822c5e
CSDUser <prokudina_ua@mail.ru>
2026-02-08 15:08:29 +0700

    Ответ на приветствие

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Class!");
+Console.WriteLine("Hello from Visual Studio!");
+Console.WriteLine("Hello from GitHub!");
