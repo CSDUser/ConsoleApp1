f5b553978fbde54b86dd82b4981805766263f5e2
Юлия <prokudina_ua@mail.ru>
2026-02-08 15:15:56 +0700

    Добавлены восклицательные знаки

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello from Visual Studio!");
 Console.WriteLine("Hello from GitHub!");
+Console.WriteLine("!!!");
